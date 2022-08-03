c5dced7cf1470bc34eff82213643a72500b10922
Angelina Zhang <55762292+ayz122004@users.noreply.github.com>
2022-08-03 16:03:40 -0700

    add http methods

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import React, { useState } from "react";
 
 function App() {
+  const [bikeStatus, setBikeStatus] = useState("");
+  const [bikeTheftButton, setBikeTheftButton] = React.useState(null);
+
+  function getBikeTheftButton() {
+    axios
+      .post(
+        "https://io.adafruit.com/api/v2/webhooks/feed/JCByxuvRpTcYKB9sdvcAuEyt1fGe",
+        {
+          value: "hello",
+        }
+      )
+      .then((response) => {
+        setBikeTheftButton(response.data);
+      });
+  }
+
+  const getBikeStatus = () => {
+    axios
+      .get("https://io.adafruit.com/api/v2/lmarielle/feeds/bikestatus")
+      .then((response) => {
+        console.log(response);
+        setBikeStatus("last value: " + response.data.last_value);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Dashboard</h1>
+        <p>Bike Theft Detector</p>
+        <button onClick={getBikeStatus}>Get Bike Status</button>
+        {bikeStatus}
+        <button onClick={getBikeTheftButton}>toggle bike theft button</button>
       </header>
     </div>
   );
